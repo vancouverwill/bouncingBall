2dbb0f6fe58d3b6f1255d98e9b3e194b2d28a304
Will Melbourne <willmelbourne@gmail.com>
2016-02-26 12:12:46 +0000

    succesfully importing js files

--- gulpfile.js
+++ gulpfile.js
@@ -6,14 +6,19 @@ var uglify = require('gulp-uglify');
 var assign = require('lodash.assign');
 var babelify = require('babelify');
 var browserify = require('browserify');
+var gulpbrowserify = require('gulp-browserify');
 var watchify = require('watchify');
 
 var gutil = require('gulp-util');
 var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
 
+var expect = require('gulp-expect-file');
 
-gulp.task('babel', function() {
+var plumber = require('gulp-plumber');
+
+
+/*gulp.task('babel', function() {
 	return gulp.src('src/index.js', { base: 'src' })
 		.pipe(sourcemaps.init())
 		.pipe(babel({
@@ -22,19 +27,33 @@ gulp.task('babel', function() {
     .pipe(uglify())
 		.pipe(sourcemaps.write("."))
 		.pipe(gulp.dest('build'));
-});
+});*/
 
 // gulp.task('watch-js', function() {  
 //   gulp.watch('./src/*.js' , ['babel']);
 // });
+// 
+// 
+
+
+// Basic usage 
+/*gulp.task('scripts', function() {
+  // Single entry point to browserify 
+  gulp.src('src/index.js')
+    .pipe(browserify({
+      insertGlobals : true,
+      debug : true
+    }))
+    .pipe(gulp.dest('./build/js'))
+});*/
 
 
 
 
 // add custom browserify options here
 var customOpts = {
-  entries: ['build/index.js'],
-  debug: true
+  entries: ['src/index.js'],
+  debug: false
 };
 var opts = assign({}, watchify.args, customOpts);
 var bundler = watchify(browserify(opts)); 
@@ -42,31 +61,35 @@ var bundler = watchify(browserify(opts));
 // add transformations here
 // i.e. b.transform(coffeeify);
 // Babel transform
-bundler.transform(babelify.configure({
-    sourceMapRelative: 'build'
-}));
+// bundler.transform(babelify.configure({
+//     sourceMapRelative: 'src'
+// }));
 
-gulp.task('watchify', bundle); // so you can run `gulp js` to build the file
+gulp.task('js', bundle); // so you can run `gulp js` to build the file
 bundler.on('update', bundle); // on any dep update, runs the bundler
 bundler.on('log', gutil.log); // output build logs to terminal
 
 function bundle() {
   return bundler.bundle()
     // log errors if they happen
-    .on('error', function (err) {
-    	gutil.log.bind(gutil, 'Browserify Error')
-    	this.emit("end");
-    })
-    .pipe(source('./dist/bundle.js'))
+    // .pipe(plumber())
+    .pipe(source('bundle.js'))
     // optional, remove if you don't need to buffer file contents
     .pipe(buffer())
     // optional, remove if you dont want sourcemaps
     .pipe(sourcemaps.init({loadMaps: true})) // loads map from browserify file
+    // .pipe(uglify())
+    .on('error', function (err) {
+      gutil.log.bind(gutil, 'Browserify Error')
+      // this.emit("end");
+    })
        // Add transformation tasks to the pipeline here.
     .pipe(sourcemaps.write('./')) // writes .map file
-    .pipe(gulp.dest('./'));
+    .pipe(gulp.dest('./dist'))
+    // .pipe(expect('dist/bundle.js'));
 }
 
 
 // gulp.task('default', ['babel', 'watch-js', 'watchify']);  
-gulp.task('default', ['babel', 'watchify']);  
+// gulp.task('default', ['babel', 'watchify']);  
+gulp.task('default', ['js']);  
--- src/index.js
+++ src/index.js
@@ -1,166 +1,30 @@
 /* exported bodySetUp */
 "use strict";
 
-// require('babel-core/register');
+
+var PageSetUp = require('./PageSetUp');
 
 
-var myCanvas, canvasHeight, canvasWidth;
-
-class PageSetUp {
-	static setWidth() {
-		myCanvas.width = canvasWidth;
-	}
-
-	static setHeight() {
-		myCanvas.height = canvasHeight;
-	}
-
-	static setCanvasSize() {
-		canvasHeight = document.body.clientHeight;
-		canvasWidth = document.body.clientWidth;
-		myCanvas = document.getElementById("myCanvas");  
-		PageSetUp.setWidth();
-		PageSetUp.setHeight();
-	}
-}
+exports.myCanvas = document.getElementById("myCanvas"); 
 
 var bodySetUp = function() {
+	// var pageSetUp = PageSetUp();
 	PageSetUp.setCanvasSize();
-	var canvas = new Canvas();
-	myCanvas.addEventListener("click", canvas.addBall.bind(canvas), false);
+	PageSetUp.addEvents();
 };
 
-window.onresize = PageSetUp.setCanvasSize;
+window.onload = function () {
+  	bodySetUp();
+}
 
-
-let instance = null;
-
-class Canvas {
-	constructor() {
-		if(!instance){
-			instance = this;
-			this.animation_active = false;
-		}
-
-		this.balls = [];
-		this.context = myCanvas.getContext("2d");
-		// this.refreshIntervalId;
-		this.raf;
-
-		return instance;
-	}
-
-	addBall(event) {
-		var ball = new BouncingBall(event.clientX, event.clientY);
-		this.balls.push(ball);
-
-		this.setAnimating();
-	}
-
-	setAnimating() {
-		if (this.animation_active === false) {
-			this.animate();
-			this.animation_active = true;
-		}
-	}
-
-	animate(){
-		this.context.clearRect(0, 0, canvasWidth, canvasHeight);
-		
-		this.balls = this.balls.map(function(ball, index) {
-			ball.updatePosition();
-			this.drawBall(ball);
-				return ball;
-		}, this);
-
-		this.balls = this.balls.filter(this.isInXAxis);
-
-		if (this.balls.length == 0) {
-			window.cancelAnimationFrame(this.raf);
-			this.animation_active = false;
-			return;
-		}
-
-		this.raf = window.requestAnimationFrame(this.animate);
-		// this.raf = window.requestAnimationFrame(this.draw.bind(this));
-	}
-
-	isInXAxis(ball) {
-		if ( ball.x > canvasWidth + ball.pointRadius) {
-			return false;
-		}
-		return true;
-	}
-
-	drawBall(ball) {
-		this.context.beginPath();
-		this.context.fillStyle= ball.colour;
-		this.context.arc(ball.x, ball.y, ball.pointRadius, 0, Math.PI*2, true);
-		this.context.closePath();
-		this.context.fill();
-	}
+window.onresize = function() {
+	PageSetUp.setCanvasSize();
 }
 
 
-class  BouncingBall {
-	constructor(startingX, startingY) {
-		
-		this.x = startingX;
-		this.y = startingY;
 
-		this.colour = this.getRandomColor();
-		this.dx = Math.random() * 2 + 0.5;
-		this.dy = Math.random() * -8 - 1;
 
-		this.pointRadius = 5;
 
-		this.gravityRate = 0.1;
-		this.dampingRate = 0.8;
 
-		this.colour;
-	}
 
-	set y(value) {
-		if (typeof value !== 'number') {
-			throw new Error('"y" must be a number.');
-		}
-		this._y = value;
-	}
 
-	get y() {
-		return this._y;
-	}  
-
-	set x(value) {
-		if (typeof value !== 'number') {
-			throw new Error('"x" must be a number.');
-		}
-		this._x = value;
-	}
-
-	get x() {
-		return this._x;
-	}
-
-	updatePosition() {
-		if (this.y > canvasHeight) {
-			this.y = canvasHeight;
-			this.dy = -this.dy * this.dampingRate; 
-		}
-		this.x += this.dx; 
-		this.y += this.dy;
-
-		this.dy = this.dy + this.gravityRate;
-	}
-
-	getRandomColor() {
-		var letters = "0123456789ABCDEF".split("");
-		var color = "#";
-		for (var i = 0; i < 6; i++ ) {
-			color += letters[Math.floor(Math.random() * 16)];
-		}
-		return color;
-	}	
-}
-
-var exports = module.exports = BouncingBall;
