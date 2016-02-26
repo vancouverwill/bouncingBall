97de795845a39ad1acadf4ff879b726162b8bde2
Will Melbourne <willmelbourne@gmail.com>
2016-02-26 16:57:31 +0000

    built bundle.js pt2

---
+++ test/Canvas.js
@@ -0,0 +1,31 @@
+require('jsdom-global')()
+
+var assert = require('assert');
+
+var Canvas = require('../../src/Canvas');
+
+
+describe('Canvas', function() {
+  var canvas;
+
+  before(function() {
+  	// var html = '<div></div><div></div><div class="addHere"></div>';
+  	// $('body').append(html);
+    
+    var PageSetUp = require("./PageSetUp");
+    var htmlCanvas = document.getElementById("myCanvas"); 
+    canvas = new Canvas(htmlCanvas);
+  });
+
+  // beforeEach(function() {
+  // ball = new BouncingBall(20, 40);
+  // });
+  // 
+  
+    it('should have 0 balls to start with', function () {
+
+        assert.equal(0, canvas.balls.length);
+    });
+  
+
+ });
